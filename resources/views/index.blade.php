8ca53d1653dd162c9d83dbae20998ed2c6a2528a
yugo uchiida <y.uchiida@nirneege.com>
2022-12-12 22:22:04 +0900

    feature: トップページに記事タイトル一覧を表示

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.index')
+
+@section('content')
+    <h1>ブログ一覧</h1>
+    <ul>
+        @foreach ($posts as $index => $post)
+            <li>{{ $post->title }}</li>
+        @endforeach
+    </ul>
+@endsection
---
+++ tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostListControllerTest extends TestCase
+{
+    // テストでデータベースを使う場合、マイグレーション処理などを行うために必要
+    use RefreshDatabase;
+
+    /** @test */
+    function トップページにアクセスし、Postタイトルが表示されている()
+    {
+        // 1. 準備
+        $post1 = Post::factory()->create(['title' => 'post title1']);
+        $post2 = Post::factory()->create(['title' => 'post title2']);
+
+        // 2. テスト対象の処理を実行
+        $response = $this->get('/');
+
+        // 3. 検証 (Post のタイトルが表示されているか)
+        $response->assertOk(); // ページが取得できてることをまず確認する
+        $response->assertSee('post title1');
+        $response->assertSee('post title2');
+    }
+}

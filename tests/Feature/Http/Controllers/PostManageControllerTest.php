468fa21c454e00f2137b12b963e216ff8d5f5649
yugo uchiida <y.uchiida@nirneege.com>
2022-12-14 17:16:36 +0900

    認証済みの場合はマイページにアクセスできる(空ページ)

--- tests/Feature/Http/Controllers/PostManageControllerTest.php
+++ tests/Feature/Http/Controllers/PostManageControllerTest.php
@@ -2,17 +2,41 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostManageControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
     /** @test */
     public function ユーザー認証済みでなければ、マイページを表示できない()
     {
         // 未ログイン状態の場合はログインページに遷移する
         $response = $this->get(route('mypage:posts'));
         $response->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function ユーザー認証済みの場合はマイページを表示する()
+    {
+        $this->actingAs($this->user);
+
+        $response = $this->get(route('mypage:posts'));
+        $response->assertOk();
     }
 }
